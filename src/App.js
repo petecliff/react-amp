83e844b6d62efe637110fde531aa601db1c51a4e
Peter Cliff <peter.cliff@jisc.ac.uk>
2020-12-02 10:14:54 +0000

    Remove poem

--- src/App.js
+++ src/App.js
@@ -17,22 +17,6 @@ function App() {
         >
           Learn React
         </a>
-    <p>
-    I’ve tried the new moon tilted in the air<br />
-Above a hazy tree-and-farmhouse cluster<br />
-As you might try a jewel in your hair.<br />
-I’ve tried it fine with little breadth of luster,<br />
-Alone, or in one ornament combining<br />
-With one first-water star almost as shining.<br />
-<br />
-I put it shining anywhere I please.<br />
-By walking slowly on some evening later,<br />
-I’ve pulled it from a crate of crooked trees,<br />
-And brought it over glossy water, greater,<br />
-And dropped it in, and seen the image wallow,<br />
-The color run, all sorts of wonder follow.<br />
-    </p>
-
       </header>
     </div>
   );
